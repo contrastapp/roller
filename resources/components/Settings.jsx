bb3c304f192411ff53f29eb7e07fb21ad06dabe3
Jono Kolnik <jono@toyboxsystems.com>
2018-05-31 13:38:00 -0700

    mock settings

--- resources/components/Settings.jsx
+++ resources/components/Settings.jsx
@@ -12,7 +12,7 @@ class Layer extends React.Component {
   }
 
   componentDidMount() {
-    pluginCall('setRules')
+    // pluginCall('setRules')
   }
 
   handleSubmit(data) {
--- resources/webview.js
+++ resources/webview.js
@@ -59,43 +59,40 @@ let page = 0
 let pages = 1
 
 //// WEB
-// ReactDOM.render(
-//   <Provider store={store}>
-//     { /* ConnectedRouter will use the store from Provider automatically */ }
-//     <ConnectedRouter history={history}>
-//       <div>
-//         <Route path="/" component={SettingsContainer}/>
-//         <Route exact path="/settings" component={SettingsContainer}/>
-//       </div>
-//     </ConnectedRouter>
-//   </Provider>,
-//   document.getElementById('root')
-// )
-// store.dispatch(layerActions.setLayers(mockData))
-// // analytics.identify('jono@toyboxsystems.com');
-// let colors = ["#660000", "#990000", "#cc0000", "#cc3333", "#ea4c88", "#993399", "#663399", "#333399", "#0066cc", "#0099cc", "#66cccc", "#77cc33", "#669900", "#336600", "#666600", "#999900", "#cccc33", "#ffff00", "#ffcc33", "#ff9900", "#ff6600", "#cc6633", "#996633", "#663300", "#000000", "#999999", "#cccccc", "#ffffff"]
-
-// data = [{ name: 'blue', hex: '#d3d3d3'
-// }
-// ]
-
-// store.dispatch(ruleActions.setColors(data))
-
-////SKETCH
-pluginCall("getLocation")
 ReactDOM.render(
   <Provider store={store}>
     { /* ConnectedRouter will use the store from Provider automatically */ }
     <ConnectedRouter history={history}>
       <div>
-        <Route exact path="/list" component={AppContainer}/>
+        <Route path="/" component={SettingsContainer}/>
         <Route exact path="/settings" component={SettingsContainer}/>
-        <Redirect to={window.redirectTo} />
       </div>
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 )
+store.dispatch(layerActions.setLayers(mockData))
+// analytics.identify('jono@toyboxsystems.com');
+let colors = ["#660000", "#990000", "#cc0000", "#cc3333", "#ea4c88", "#993399", "#663399", "#333399", "#0066cc", "#0099cc", "#66cccc", "#77cc33", "#669900", "#336600", "#666600", "#999900", "#cccc33", "#ffff00", "#ffcc33", "#ff9900", "#ff6600", "#cc6633", "#996633", "#663300", "#000000", "#999999", "#cccccc", "#ffffff"]
+
+data = [{ name: 'blue', hex: '#d3d3d3' } ]
+store.dispatch(ruleActions.setColors(data))
+
+////SKETCH
+// pluginCall("getLocation")
+// ReactDOM.render(
+//   <Provider store={store}>
+//     { /* ConnectedRouter will use the store from Provider automatically */ }
+//     <ConnectedRouter history={history}>
+//       <div>
+//         <Route exact path="/list" component={AppContainer}/>
+//         <Route exact path="/settings" component={SettingsContainer}/>
+//         <Redirect to={window.redirectTo} />
+//       </div>
+//     </ConnectedRouter>
+//   </Provider>,
+//   document.getElementById('root')
+// )
 // let colors = ["#660000", "#990000", "#cc0000", "#cc3333", "#ea4c88", "#993399", "#663399", "#333399", "#0066cc", "#0099cc", "#66cccc", "#77cc33", "#669900", "#336600", "#666600", "#999900", "#cccc33", "#ffff00", "#ffcc33", "#ff9900", "#ff6600", "#cc6633", "#996633", "#663300", "#000000", "#999999", "#cccccc", "#ffffff"]
 // store.dispatch(ruleActions.setColors(colors))
 
